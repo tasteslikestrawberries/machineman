702da75b475992839d1ba5b3f8daa506f8db1660
tasteslikestrawberries <anja.mustac@gmail.com>
2021-02-08 22:37:19 +0100

    up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react"
+import Button from '@material-ui/core/Button';
 
 const Header = () => {
   return (
     <header>
       <h1>This is my first React App!</h1>
       <p>learning React</p>
+      <Button variant="contained" color="primary">
+      Hello World!
+    </Button>
     </header>
   )
 }
