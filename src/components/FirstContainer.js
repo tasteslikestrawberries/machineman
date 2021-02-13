c29441483b62ca5bf1c9fb4e78fffca57ff4ac35
tasteslikestrawberries <anja.mustac@gmail.com>
2021-02-13 15:22:18 +0100

    up

--- src/components/FirstContainer.js
+++ src/components/FirstContainer.js
@@ -13,14 +13,14 @@ class FirstContainer extends React.Component {
             <div className='content1'>
                
             “When I used to read fairy tales, I fancied that kind of thing never happened, and now here I am in the middle of one!” 
-
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
             ullamco laboris nisi ut aliquip ex ea commodo consequat. 
             Duis aute irure dolor in reprehenderit in voluptate velit 
             esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
             occaecat cupidatat non proident, sunt in culpa qui officia 
-            deserunt moliiiiit anim id est laborum.  
+            deserunt moliiiiit anim id est laborum.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+           aliqua.
          
             </div>
 
--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Info extends React.Component {
     render() {
       return <div className='info'>
-      designed by <a id="taste" href='mailto: tasteslikestrawberries.design@gmail.com'> 
+      &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;designed by <a id="taste" href='mailto: tasteslikestrawberries.design@gmail.com'> 
       &nbsp; &nbsp;tasteslikestrawberries&nbsp; &nbsp;</a><span className='vl'></span></div> ;
     }
   }
